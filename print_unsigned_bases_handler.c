f00f9d84f81afcfd655c8961774d58a0e7792c06
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:17:35 +0200

    refactor print_unsigned, print_octal, print_hex, print_hex_lower, and print_hex_upper functions to accept print_buffer_t argument for improved output handling, update documentation to clarify return values and error management

--- print_unsigned_bases_handler.c
+++ print_unsigned_bases_handler.c
@@ -3,10 +3,11 @@
 /**
  * print_unsigned - print unsigned int
  * @ap: argument list
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_unsigned(va_list ap)
+int print_unsigned(va_list ap, print_buffer_t *pb)
 {
 	unsigned int n;
 	unsigned int divisor;
@@ -18,17 +19,23 @@ int print_unsigned(va_list ap)
 	result = 0;
 
 	if (n == 0)
-		return (_putchar('0'));
+	{
+		if (pb_putc(pb, '0') < 0)
+			return (-1);
+		return (1);
+	}
 
 	while ((n / divisor) > 9)
-		divisor *= 10;
+		divisor = divisor * 10;
 
 	while (divisor != 0)
 	{
 		digit = n / divisor;
-		result += _putchar(digit + '0');
-		n %= divisor;
-		divisor /= 10;
+		if (pb_putc(pb, (char)(digit + '0')) < 0)
+			return (-1);
+		result = result + 1;
+		n = n % divisor;
+		divisor = divisor / 10;
 	}
 
 	return (result);
@@ -38,10 +45,11 @@ int print_unsigned(va_list ap)
 /**
  * print_octal - print unsigned int in octal
  * @ap: argument list
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_octal(va_list ap)
+int print_octal(va_list ap, print_buffer_t *pb)
 {
 	unsigned int n;
 	char buf[32];
@@ -53,18 +61,25 @@ int print_octal(va_list ap)
 	len = 0;
 
 	if (n == 0)
-		return (_putchar('0'));
+	{
+		if (pb_putc(pb, '0') < 0)
+			return (-1);
+		return (1);
+	}
 
 	while (n != 0)
 	{
-		buf[i] = (n % 8) + '0';
-		n /= 8;
-		i++;
+		buf[i] = (char)((n % 8) + '0');
+		n = n / 8;
+		i = i + 1;
 	}
 
 	while (i > 0)
 	{
-		len += _putchar(buf[--i]);
+		i = i - 1;
+		if (pb_putc(pb, buf[i]) < 0)
+			return (-1);
+		len = len + 1;
 	}
 
 	return (len);
@@ -74,10 +89,11 @@ int print_octal(va_list ap)
  * print_hex - print unsigned int in hexadecimal
  * @ap: argument list
  * @digits: symbols used for conversion
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-static int print_hex(va_list ap, const char *digits)
+static int print_hex(va_list ap, const char *digits, print_buffer_t *pb)
 {
 	unsigned int n;
 	char buf[16];
@@ -89,17 +105,26 @@ static int print_hex(va_list ap, const char *digits)
 	len = 0;
 
 	if (n == 0)
-		return (_putchar('0'));
+	{
+		if (pb_putc(pb, '0') < 0)
+			return (-1);
+		return (1);
+	}
 
 	while (n != 0)
 	{
 		buf[i] = digits[n % 16];
-		n /= 16;
-		i++;
+		n = n / 16;
+		i = i + 1;
 	}
 
 	while (i > 0)
-		len += _putchar(buf[--i]);
+	{
+		i = i - 1;
+		if (pb_putc(pb, buf[i]) < 0)
+			return (-1);
+		len = len + 1;
+	}
 
 	return (len);
 }
@@ -107,21 +132,23 @@ static int print_hex(va_list ap, const char *digits)
 /**
  * print_hex_lower - print unsigned int in hexadecimal lowercase
  * @ap: argument list
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_hex_lower(va_list ap)
+int print_hex_lower(va_list ap, print_buffer_t *pb)
 {
-	return (print_hex(ap, "0123456789abcdef"));
+	return (print_hex(ap, "0123456789abcdef", pb));
 }
 
 /**
  * print_hex_upper - print unsigned int in hexadecimal uppercase
  * @ap: argument list
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_hex_upper(va_list ap)
+int print_hex_upper(va_list ap, print_buffer_t *pb)
 {
-	return (print_hex(ap, "0123456789ABCDEF"));
+	return (print_hex(ap, "0123456789ABCDEF", pb));
 }
