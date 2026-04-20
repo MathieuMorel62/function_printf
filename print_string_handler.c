30097417383584e2b90b5452adc74c774f61bdc4
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:17:29 +0200

    refactor print_string function to accept print_buffer_t argument for improved output handling, update documentation to clarify return value and error management

--- print_string_handler.c
+++ print_string_handler.c
@@ -3,19 +3,27 @@
 /**
  * print_string - prints a string
  * @ap: argument list
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_string(va_list ap)
+int print_string(va_list ap, print_buffer_t *pb)
 {
-	int i;
-	char *str = va_arg(ap, char *);
+	int index;
+	int count;
+	char *str;
 
+	str = va_arg(ap, char *);
 	if (str == NULL)
 		str = "(null)";
 
-	for (i = 0; str[i]; i++)
-		_putchar(str[i]);
+	count = 0;
+	for (index = 0; str[index] != '\0'; index = index + 1)
+	{
+		if (pb_putc(pb, str[index]) < 0)
+			return (-1);
+		count = count + 1;
+	}
 
-	return (i);
+	return (count);
 }
