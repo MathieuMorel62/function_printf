0476946b9095368be59f89cedbf60a6ee0bdc084
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:15:52 +0200

    implement apply_specifier function to handle '%' conversions in _printf, improving format processing and output buffer management

--- _printf.c
+++ _printf.c
@@ -1,6 +1,55 @@
 #include "main.h"
 
 /**
+ * apply_specifier - handle a '%' conversion starting at *pi
+ * @format: format string
+ * @pi: index of '%'; updated after processing
+ * @plen: running length; updated
+ * @ap: variadic arguments
+ * @pb: output buffer
+ *
+ * Return: 0 on success, -1 on I/O error, -2 if format ends after '%'
+ */
+static int apply_specifier(const char *format, int *pi, int *plen,
+		va_list ap, print_buffer_t *pb)
+{
+	int i = *pi;
+	int len = *plen;
+	int r;
+	int (*func)(va_list ap, print_buffer_t *pb);
+
+	if (format[i + 1] == '\0')
+		return (-2);
+	i = i + 1;
+	if (format[i] != '\0')
+	{
+		func = get_function(format + i);
+		i = i + 1;
+	}
+	else
+		func = NULL;
+	if (func == NULL)
+	{
+		if (pb_putc(pb, '%') < 0)
+			return (-1);
+		len = len + 1;
+		if (pb_putc(pb, format[i - 1]) < 0)
+			return (-1);
+		len = len + 1;
+	}
+	else
+	{
+		r = func(ap, pb);
+		if (r < 0)
+			return (-1);
+		len = len + r;
+	}
+	*pi = i;
+	*plen = len;
+	return (0);
+}
+
+/**
  * _printf - function printf
  * @format: character string
  *
@@ -10,41 +59,45 @@ int _printf(const char *format, ...)
 {
 	int i = 0;
 	int len = 0;
-	int (*func)(va_list ap);
+	int r;
 	va_list ap;
+	print_buffer_t pb;
 
+	pb.used = 0;
 	va_start(ap, format);
-
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		va_end(ap);
 		return (-1);
-
+	}
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
+			r = apply_specifier(format, &i, &len, ap, &pb);
+			if (r == -2 || r < 0)
+			{
+				(void)pb_flush(&pb);
+				va_end(ap);
 				return (-1);
-			i++;
-		if (format[i] != '\0')
-		{
-			func = get_function(format + i);
-			i++;
-		}
-		if (func == NULL)
-		{
-			_putchar('%'),
-			_putchar(format[i - 1]),
-			len += 2;
-		}
-		else
-			len += func(ap);
+			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			len++;
-			i++;
+			if (pb_putc(&pb, format[i]) < 0)
+			{
+				(void)pb_flush(&pb);
+				va_end(ap);
+				return (-1);
+			}
+			len = len + 1;
+			i = i + 1;
 		}
+	}
+	if (pb_flush(&pb) < 0)
+	{
+		va_end(ap);
+		return (-1);
 	}
 	va_end(ap);
 	return (len);
