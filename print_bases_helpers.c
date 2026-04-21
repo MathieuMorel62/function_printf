94d8152e3ff983df2afe6c48fa85143eae352e31
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-21 13:39:51 +0200

    add print buffer helper functions for reversed output and hex prefix handling; implement write_reversed_buffer and write_hex_prefix_if_needed to enhance formatting capabilities

---
+++ print_bases_helpers.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * write_reversed_buffer - print reversed temporary digits buffer
+ * @pb: output buffer
+ * @buf: temporary digits buffer
+ * @size: number of valid chars in buf
+ *
+ * Return: number of characters printed, or -1 on error
+ */
+int write_reversed_buffer(print_buffer_t *pb, char *buf, int size)
+{
+	int len;
+
+	len = 0;
+	while (size > 0)
+	{
+		size = size - 1;
+		if (pb_putc(pb, buf[size]) < 0)
+			return (-1);
+		len = len + 1;
+	}
+	return (len);
+}
+
+/**
+ * write_hex_prefix_if_needed - print 0x/0X prefix for # flag
+ * @pb: output buffer
+ * @original_value: original numeric value
+ *
+ * Return: prefix length, 0 when not printed, or -1 on error
+ */
+int write_hex_prefix_if_needed(print_buffer_t *pb, unsigned int original_value)
+{
+	if (pb->flag_hash != 1 || original_value == 0)
+		return (0);
+	if (pb->conversion_specifier == 'x')
+	{
+		if (pb_putc(pb, '0') < 0 || pb_putc(pb, 'x') < 0)
+			return (-1);
+		return (2);
+	}
+	if (pb->conversion_specifier == 'X')
+	{
+		if (pb_putc(pb, '0') < 0 || pb_putc(pb, 'X') < 0)
+			return (-1);
+		return (2);
+	}
+	return (0);
+}
